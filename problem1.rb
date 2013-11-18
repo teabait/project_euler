d8b88e568037266055463a400b5dc5fba95d5cd1
teabait <hello.teaho@gmail.com>
2013-11-18 09:48:49 -0500

    problem 1 dynamic

--- problem1.rb
+++ problem1.rb
@@ -1,6 +1,13 @@
-
 multiples = (0..999).select do |i|
   i % 3 === 0 || i % 5 === 0
 end
 
 multiples.inject{|sum,x| sum + x }
+
+def calculate(min, max, num1, num2)
+  multiples = (min..max).select do |i|
+    i % num1 === 0 || i % num2 === 0
+  end
+
+  multiples.inject{|sum,x| sum + x }
+end
