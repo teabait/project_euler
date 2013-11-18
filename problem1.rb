f6c5e14a9fb44367df6b32dd53b54012ec61dbe1
teabait <hello.teaho@gmail.com>
2013-11-18 09:49:47 -0500

    MOAR DOTS

--- problem1.rb
+++ problem1.rb
@@ -1,11 +1,11 @@
-multiples = (0..999).select do |i|
+multiples = (0...1000).select do |i|
   i % 3 === 0 || i % 5 === 0
 end
 
 multiples.inject{|sum,x| sum + x }
 
 def calculate(min, max, num1, num2)
-  multiples = (min..max).select do |i|
+  multiples = (min...max).select do |i|
     i % num1 === 0 || i % num2 === 0
   end
 
