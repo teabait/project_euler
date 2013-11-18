2c3876d2a8ba9560cbe3239ac3d582f2b834d095
teabait <hello.teaho@gmail.com>
2013-11-18 09:50:43 -0500

    comment out hard code

--- problem1.rb
+++ problem1.rb
@@ -1,8 +1,8 @@
-multiples = (0...1000).select do |i|
-  i % 3 === 0 || i % 5 === 0
-end
+# multiples = (0...1000).select do |i|
+#   i % 3 === 0 || i % 5 === 0
+# end
 
-multiples.inject{|sum,x| sum + x }
+# multiples.inject{|sum,x| sum + x }
 
 def calculate(min, max, num1, num2)
   multiples = (min...max).select do |i|
